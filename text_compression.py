1eea44b4149cd5a591ac1a0620639a09bc024aa0
Matthew Ognibene <mattognibene1@gmail.com>
2017-12-16 13:28:07 -0500

    added UI and refactored. also fixed the compression part (accidently made it expand the file last commit oops

--- text_compression.py
+++ text_compression.py
@@ -1,18 +1,6 @@
 import math
 import pickle
-#file_name = input("Enter file name")
 
-file = open("smp.txt", "r")
-content = file.readlines()
-file.close()
-#todo make these functions like a good programmer
-dict = {}
-for i in range(len(content)):
-    for letter in content[i]:
-        if letter in dict:
-            dict[letter] = dict[letter]+1
-        else:
-            dict[letter] = 1
 
 #A Branch is one of the following
 # - String
@@ -28,89 +16,80 @@ class split_node(object):
         return split_node(self.left,self.right)
 
 
-
-#list of Pair -> Branch
-#returns the huffman tree representation of the given dict
-#the list must be sorted
-def create_tree(pairs):
-    if len(pairs)==0:
-        return -1
-    to_be_added = []
-    next = pairs[0][1]
-    #todo helper?
-    for x in pairs:
-        if (x[1] == next):
-            to_be_added.append(x)
-
-    pairs=pairs[len(to_be_added):]
-
-    return split_node(create_branch_from_list(to_be_added),
-               create_tree(pairs))
+# String -> Dict
+# returns a dict where the key is the character and the value is how many time it occurs
+def create_occurences(content):
+    occ = {}
+    for i in range(len(content)):
+        for letter in content[i]:
+            if letter in occ:
+                occ[letter] = occ[letter] + 1
+            else:
+                occ[letter] = 1
+    return occ
 
 
-# [List-of Pairs] -> Branch
-# creates a branch from a given list of numbers where each leaf is on the same layer
-def create_branch_from_list(tba):
-    if (len(tba) == 1):
-        return tba[0][0]
-    else:
-        return split_node(
-            create_branch_from_list(first_half(tba)),
-            create_branch_from_list(second_half(tba))
-        )
+#A pair is a pair(Any Number)
+class pair(object):
+    def __init__(self, val, num):
+        self.val = val
+        self.num = num
 
-    # List -> List
-    # todo purpose
-def first_half(l):
-    return l[:math.floor(len(l) / 2)]
+# A split_node is a split_node(Branch Branch)
+class split_node(object):
+    def __init__(self, left, right):
+        self.left = left
+        self.right = right
 
-def second_half(l):
-    return l[math.floor(len(l) / 2):]
+    def __copy__(self):
+        return split_node(self.left, self.right)
 
 
+# List of Pairs -> Tree
+#takes a dict of the occurences and creates a huffman tree
+def create_tree(lop):
+    while len(lop)>1:
+        lop = combine_lowest(lop)
 
+    return lop[0].val
 
-
-
-
-
-#Dict -> Dict
-#returns a dict where each component is value of the component in d divided by the sum
-def find_probabilities(d):
-    probabilities = {}
-    for component in d:
-        probabilities[component] = d[component]/sum_dict(d)
-    return probabilities
-
-#Dict -> Int
-#sums all the numbers associated in the given dict
-def sum_dict(d):
-    sum = 0
-    for component in d:
-        sum+=d[component]
-    return sum
-
-#Dict -> New Dict
-#sorts a dict with the largest first
-#todo, use something better than a linear sort
-def sort_dict(d):
-    temp = {}
-    for comp in d:
-        temp[comp] = d[comp]
-    sorted = {}
+#LoP -> Lop
+#sorts a list of pair
+def sort_list_of_pair(lop):
+    #please stop being lazy and make this merge sort
+    temp = []
+    for i in lop:
+        temp.append(i)
+    sorted = []
     for i in (range(len(temp))):
-        largest = next(iter(temp))
+        smallest = temp[0]
+        index=0
         for it in temp:
-            if temp[it] > temp[largest]:
-                largest = it
-        sorted[largest] = dict[largest]
-        del temp[largest]
+            if it.num < smallest.num:
+                smallest = it
+                index=temp.index(smallest)
+
+        sorted.append(smallest)
+        del temp[index]
     return sorted
 
-def merge_two_dicts(x, y):
-    z = x.copy()
-    z.update(y)
-    return z
+#List of Pair -> Lst of Pair
+#takes a list of pair but with the two lowest pairs combined into a tree
+def combine_lowest(lop):
+    sorted =sort_list_of_pair(lop)
+    tree=split_node(sorted[1].val,sorted[0].val)
+    sorted.append(pair(tree,(sorted[0].num+sorted[1].num)))
+    del sorted[0]
+    del sorted[0]
+    return sorted
+
+
+# Dict -> List of Pairs
+def create_pairs(occ):
+    lop = []
+    for omega in occ:
+        lop.append(pair(omega,occ[omega]))
+    return lop
 
 #Branch String-> Dict
 #returns a dict where the key is the letter and the value is its encoding
@@ -118,58 +97,34 @@ def merge_two_dicts(x, y):
 def encode_tree(b,acc):
     if isinstance(b,str):
         return{b:acc}
-    elif b==-1:
+    elif b==None:
         return {}
     else:
         return merge_two_dicts(encode_tree(b.left, acc+"0"),
                                encode_tree(b.right, acc+"1"))
 
-huffman = create_tree(list(sort_dict(find_probabilities(dict)).items()))
-encodement = encode_tree(huffman, "")
-final = ""
-for i in range(len(content)):
-    for letter in content[i]:
-        final+=encodement[letter]
+def merge_two_dicts(x, y):
+    z = x.copy()
+    z.update(y)
+    return z
 
-def pad_zeros(b):
-    while len(b) < 8:
-        b = b + "0"
-    return b
-def pad_zeros_before(b):
-    while len(b) < 8:
-        b =  "0"  + b
-    return b
-
-
-#todo make al this shit into function
-bytes = []
-q=0
-#please fix this hack
-while q<len(final):
-    if q+8>len(final):
-        bytes.append(pad_zeros(final[q:]))
-        bytes.append(pad_zeros_before(bin(len(final[q:])-1)[2:]))
-        q=len(final)
-    else:
-        bytes.append(final[q:q+8])
-        q=q+8
-
-ints = []
-for i in bytes:
-    ints.append(int(i, 2))
-data = []
-for i in ints:
-    data.append(bytearray([i]))
-new_file = open('out.bin', 'wb')
-
-for i in data:
-    new_file.write(i)
-
-pickle.dump(huffman, open("hf.p", "wb"))
+#binary -> String
+#converts a binary number into a string rep of it as a byte
+def bin_to_bytestring(b):
+    return pad_zeros_before(b[2:])
 
 #bytearray Tree -> String
-def decompress(ba,t):
-    bs = cleave_extra(bytearray_to_string(ba))
+def decompress():
+    file_name = input("Enter the file name (NOTE! do not include the -out.bin or -enc.p. \n"
+                      "If the file is doc-out.bin and doc-enc.p, simply enter \"doc\"")
+    t = pickle.load(open(file_name+"-enc.p","rb"))
+    bin_f = open(file_name+"-out.bin","rb")
+    ba = bin_f.read()
+    bs = ""
+    for i in ba:
+        bs+= bin_to_bytestring(bin(i))
+
+    bs = cleave_extra(bs)
     text = ""
     path=t.__copy__()
     for i in bs:
@@ -180,7 +135,9 @@ def decompress(ba,t):
         if type(path)==str:
             text+=path
             path = t.__copy__()
-    return text
+    print("Decompressed file saved as "+ file_name +".txt")
+    new_file = open(file_name+".txt","w+")
+    new_file.write(text)
 
 
 #list of bytearray->String
@@ -194,3 +151,100 @@ def bytearray_to_string(ba):
 def cleave_extra (bs):
     final_bit = int(bs[-8:],2)
     return bs[:(len(bs)- (8-final_bit) +1)-8]
+
+def pad_zeros(b):
+    while len(b) < 8:
+        b = b + "0"
+    return b
+
+def pad_zeros_before(b):
+    while len(b) < 8:
+        b = "0" + b
+    return b
+
+#Bitstring -> Bytearray
+#takes a bit string and divides them into a bytearray. The final byte is always the position
+#of the final meaningful bit of the byte before it
+def divide_to_bytes(final):
+    bytes = []
+    q = 0
+    while q < len(final):
+        if q + 8 > len(final):
+            bytes.append(pad_zeros(final[q:]))
+            bytes.append(pad_zeros_before(bin(len(final[q:]) - 1)[2:]))
+            q = len(final)
+        else:
+            bytes.append(final[q:q + 8])
+            q = q + 8
+    '''data = []
+    for i in bytes:
+        data.append(bytearray(int(i, 2)))
+    return data'''
+    ints = []
+    for i in bytes:
+        ints.append(int(i, 2))
+    data = []
+    for i in ints:
+        data.append(bytearray([i]))
+    return data
+
+
+#String Encodement -> Bitstring
+#returns the final text encoded
+def encode_text(content, encodement):
+    final = ""
+    for i in range(len(content)):
+        for letter in content[i]:
+            final += encodement[letter]
+    return final
+
+#Bytearray Huffman String-> .bin .p
+#creates two files, one of the encoded text, one one the encodement
+def output(bytarry,hf,name):
+    new_file = open(remove_tag(name) + "-out.bin", 'wb')
+    for i in bytarry:
+        new_file.write(i)
+    pickle.dump(hf, open(remove_tag(name)+"-enc.p", "wb"))
+
+#main fuction for compressing
+def compress():
+    file_name = input("Enter file name")
+    file = open(file_name, "r")
+    content = file.readlines()
+    file.close()
+    occ = create_occurences(content)
+
+    #huffman->the tree representation of th encodement
+    huffman = create_tree(create_pairs(occ))
+    #encodement->a dict where the letter is the key and its encodement the value
+    encodement = encode_tree(huffman, "")
+    encoded_text = encode_text(content,encodement)
+    #output(divide_to_bytes(encoded_text),huffman,file_name)
+    new_file = open(remove_tag(file_name)+'-out.bin', 'wb')
+    for i in divide_to_bytes(encoded_text):
+        new_file.write(i)
+
+    pickle.dump(huffman, open(remove_tag(file_name)+"-enc.p", "wb"))
+
+    print("compression succesful!")
+    #todo statistics on how much space was saved
+    print("Compression saved as " + remove_tag(file_name)+"-enc.p and " + remove_tag(file_name)+"-out.bin")
+    print("note: both files are necessary for decompression")
+
+
+#String->String
+#removes tags such as .txt and .bin from file names
+def remove_tag(str):
+    return str[0:str.index(".")]
+def main():
+    choice = input("Press [1] to compress. \n"
+                   "Press [2] to decompress")
+    while(not(choice=="1" or choice=="2")):
+        choice = input("Press [1] to compress. \n"
+                       "Press [2] to decompress")
+    if choice == "1":
+        compress()
+    else:
+        decompress()
+
+main()
